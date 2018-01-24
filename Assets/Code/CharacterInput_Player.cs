d5373742b70f69b19050b5ddd3d93018ed2c27d9
Harald <harald921@hotmail.com>
2018-01-24 08:47:06 +0100

    Add super simple movement

--- Assets/Code/CharacterInput_Player.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CharacterInput_Player : MonoBehaviour
-{ 
-
-}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Exposed data
+    [Header("Movement")]
+    [SerializeField] float _moveSpeed = 2.5f;
+
+    // References
+    Rigidbody _rigidBody;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();    
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleMovement()
+    {
+         _rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _moveSpeed;
+    }
+    
+    void HandleRotation()
+    {
+
+    }
+}
